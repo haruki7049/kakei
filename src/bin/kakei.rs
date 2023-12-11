743749b95dba1881d40166ab207e60f1a5c961d4
haruki7049 <tontonkirikiri@gmail.com>
2023-12-11 15:18:46 +0900

    fix(kakei.rs): Adding SubCommands and implementing FromStr

--- src/bin/kakei.rs
+++ src/bin/kakei.rs
@@ -4,29 +4,55 @@ fn main() {
     let args: Args = Args::parse();
     let is_listed: bool = args.list;
 
-    match args.sub_command.as_str() {
-        "set-income" => {
-            println!("set-income is running...")
-        }
-        "get-balance" => {
-            println!("{}", is_listed);
-            println!("get-balance is running...")
-        }
-        "set-expense" => {
-            println!("set-expense is running...");
-        }
-        _ => {
-            eprintln!("Unknown command: {}", &args.sub_command);
-        }
+    match args.sub_command {
+        SubCommands::SetIncome => set_income(),
+        SubCommands::SetExpense => set_expense(),
+        SubCommands::GetBalance => get_balance(&is_listed),
     }
 }
+
+fn set_income() {
+    println!("set-income command typed");
+}
+
+fn set_expense() {
+    println!("set-expense command typed");
+}
+
+fn get_balance(_is_listed: &bool) {
+    println!("get-balance command typed");
+}
+
+const SET_INCOME: &str = "set-income";
+const SET_EXPENSE: &str = "set-expense";
+const GET_BALANCE: &str = "get-balance";
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 pub struct Args {
     #[arg(help = "What you want to do")]
-    pub sub_command: String,
+    pub sub_command: SubCommands,
 
     #[arg(long, help = "If you want kakei to show as list, use --list")]
     pub list: bool,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum SubCommands {
+    SetIncome,
+    SetExpense,
+    GetBalance,
+}
+
+impl std::str::FromStr for SubCommands {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            SET_INCOME => Ok(Self::SetIncome),
+            GET_BALANCE => Ok(Self::GetBalance),
+            SET_EXPENSE => Ok(Self::SetExpense),
+            _ => Err(format!("Unknown sub command: {}", s)),
+        }
+    }
 }
