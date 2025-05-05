8e2cbda15bdfcf079ea8e1e5ff9a6884e9b1e18d
haruki7049 <tontonkirikiri@gmail.com>
2025-05-05 19:06:42 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use directories::ProjectDirs;
 use serde_derive::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Write;
@@ -20,13 +21,16 @@ fn main() -> anyhow::Result<()> {
 /// Initialize default kakeibo file.
 /// To: ~/.local/share/kakei/default.csv
 fn init_default_kakeibo() -> anyhow::Result<()> {
-    let app_name = env!("CARGO_PKG_NAME");
+    // Gets project_dirs, contains data_dir
+    let project_dirs = ProjectDirs::from("dev", "haruki7049", "kakei")
+        .expect("ERROR: Cannot read project_dirs for dev.haruki7049.kakei");
 
     // Gets /home/haruki/.local/share/kakei/default.csv
-    let kakeibo_path =
-        xdg::BaseDirectories::with_prefix(app_name).place_data_file("default.csv")?;
+    let kakeibo_path = project_dirs.data_dir().join("default.csv");
+
+    // Save default.csv to the path with default context
     let mut file = File::create(kakeibo_path)?;
-    file.write_all(b"Name,Price\nSushi,-1000")?;
+    file.write_all(b"Name,Price\nSushi,-1000\n")?;
 
     Ok(())
 }
@@ -40,6 +44,7 @@ fn init_config() -> anyhow::Result<()> {
     // Initialize config
     let mut cfg = KakeiConfig::default();
 
+    // Write kakei binary's version
     cfg.version = env!("CARGO_PKG_VERSION").to_string();
 
     // Records configuration to filesystem
