821b8d220ac608c223cc5fc58a9deebe58a544a9
haruki7049 <tontonkirikiri@gmail.com>
2026-06-17 18:57:34 +0900

    Update

--- crates/processor/src/lib.rs
+++ crates/processor/src/lib.rs
@@ -1,6 +1,23 @@
+// ----- Processor -----
+
 /// Processor trait to express the processor methods
 pub trait Processor {}
 
+#[derive(Debug)]
 pub struct KakeiProcessor {}
 
 impl Processor for KakeiProcessor {}
+
+// ----- Kakeibo -----
+
+pub trait Kakeibo {}
+
+#[derive(Debug)]
+pub struct KakeiboNote {}
+
+impl Kakeibo for KakeiboNote {}
+
+// ----- Configuration -----
+
+#[derive(Debug)]
+pub struct Configuration {}
