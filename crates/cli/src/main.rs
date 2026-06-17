e9c74c2881a8b7472137fb217683f8c60a89a861
haruki7049 <tontonkirikiri@gmail.com>
2026-06-17 18:35:25 +0900

    WIP

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,9 @@
 use clap::Parser;
+use directories::ProjectDirs;
+use std::{
+    path::PathBuf,
+    sync::{LazyLock, Mutex},
+};
 
 fn main() -> anyhow::Result<()> {
     let _ = CLIArgs::parse();
@@ -9,4 +14,20 @@ fn main() -> anyhow::Result<()> {
 
 #[derive(Debug, Parser)]
 #[clap(author, about, version)]
-pub struct CLIArgs {}
+pub struct CLIArgs {
+    kakeibo: PathBuf,
+
+    #[arg(short, long, default_value = DEFAULT_CONFIG_PATH.lock().unwrap().display().to_string())]
+    config: PathBuf,
+}
+
+/// Default Configuration Path, using directories crate to calculate ProjectDirs (~/.config/kakei)
+static DEFAULT_CONFIG_PATH: LazyLock<Mutex<PathBuf>> = LazyLock::new(|| {
+    let proj_dirs = ProjectDirs::from("dev", "haruki7049", "kakei")
+        .expect("Failed to search ProjectDirs for dev.haruki7049.kakei");
+    let mut config_path: PathBuf = proj_dirs.config_dir().to_path_buf();
+    let filename: &str = "config.toml";
+
+    config_path.push(filename);
+    Mutex::new(config_path)
+});
