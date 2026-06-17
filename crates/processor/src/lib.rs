e9a2ee388cb3d2e36eb1d8d75dd6ef8416c1d135
haruki7049 <tontonkirikiri@gmail.com>
2026-06-17 18:51:19 +0900

    Update

--- crates/processor/src/lib.rs
+++ crates/processor/src/lib.rs
@@ -1,3 +1,6 @@
-pub mod types {
-    pub trait Processor {}
-}
+/// Processor trait to express the processor methods
+pub trait Processor {}
+
+pub struct KakeiProcessor {}
+
+impl Processor for KakeiProcessor {}
