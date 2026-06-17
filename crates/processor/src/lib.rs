25b7deece0c2639b782c46c478449721d693eceb
haruki7049 <tontonkirikiri@gmail.com>
2026-06-17 20:07:07 +0900

    Update

--- crates/processor/src/lib.rs
+++ crates/processor/src/lib.rs
@@ -14,32 +14,57 @@ impl Processor for KakeiProcessor {}
 
 // ----- Kakeibo -----
 
-pub trait Kakeibo {
+pub trait Note {
     fn table(&self) -> tabled::Table;
 }
 
 #[derive(Debug)]
 pub struct KakeiboNote<C>
 where
-    C: Unit + std::fmt::Display,
+    C: Unit + std::fmt::Display + Clone,
 {
     queries: Vec<KakeiboQuery<C>>,
 }
 
-impl<C: Unit + std::fmt::Display> Kakeibo for KakeiboNote<C> {
+impl<C> KakeiboNote<C>
+where
+    C: Unit + std::fmt::Display + Clone,
+{
+    pub fn new(queries: Vec<KakeiboQuery<C>>) -> Self {
+        Self { queries }
+    }
+}
+
+impl<C> Note for KakeiboNote<C>
+where
+    C: Unit + std::fmt::Display + Clone,
+{
     fn table(&self) -> tabled::Table {
         Table::new(&self.queries)
     }
 }
 
-#[derive(Debug, Tabled)]
+#[derive(Debug, Tabled, Clone)]
 pub struct KakeiboQuery<C>
 where
-    C: Unit + std::fmt::Display,
+    C: Unit + std::fmt::Display + Clone,
 {
     pub name: String,
     pub debit: C,
     pub credit: C,
+}
+
+impl<C> KakeiboQuery<C>
+where
+    C: Unit + std::fmt::Display + Clone,
+{
+    pub fn new(name: String, debit: C, credit: C) -> Self {
+        Self {
+            name,
+            debit,
+            credit,
+        }
+    }
 }
 
 // ----- Currency & Unit -----
@@ -49,20 +74,10 @@ pub trait Unit {
     fn is_i64(&self) -> bool;
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Currency {
     JPY(JPY),
     SATS(SATS),
-}
-
-#[derive(Debug, PartialEq)]
-pub struct JPY {
-    inner: i64,
-}
-
-#[derive(Debug, PartialEq)]
-pub struct SATS {
-    inner: i64,
 }
 
 impl Currency {
@@ -78,8 +93,8 @@ impl Currency {
 impl std::fmt::Display for Currency {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::JPY(_) => write!(f, "JPY"),
-            Self::SATS(_) => write!(f, "SATS"),
+            Self::JPY(i) => write!(f, "{}", i),
+            Self::SATS(i) => write!(f, "{}", i),
         }
     }
 }
@@ -100,6 +115,30 @@ impl Unit for Currency {
     }
 }
 
+// ----- Each Currency Unit -----
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct JPY {
+    inner: i64,
+}
+
+impl std::fmt::Display for JPY {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} JPY", self.inner)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct SATS {
+    inner: i64,
+}
+
+impl std::fmt::Display for SATS {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} SATS", self.inner)
+    }
+}
+
 // ----- Configuration -----
 
 #[derive(Debug, Tabled)]
@@ -108,11 +147,46 @@ pub struct Configuration {}
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tabled::assert::assert_table;
 
     #[test]
     fn currency() -> anyhow::Result<()> {
         let _ = Currency::jpy(1); // 1 JPY
         let _ = Currency::sats(1000); // 1000 SATS
+        Ok(())
+    }
+
+    #[test]
+    fn table() -> anyhow::Result<()> {
+        let jpy_query = KakeiboQuery {
+            name: "Test JPY query".to_string(),
+            debit: Currency::jpy(1),  // 1 JPY
+            credit: Currency::jpy(0), // 0 JPY
+        };
+        let sats_query = KakeiboQuery {
+            name: "Test SATS query".to_string(),
+            debit: Currency::sats(1000), // 1000 JPY
+            credit: Currency::sats(0),   // 0 JPY
+        };
+        let queries = vec![jpy_query.clone(), jpy_query, sats_query.clone(), sats_query];
+        let kakeibo = KakeiboNote::new(queries);
+
+        let table: Table = kakeibo.table();
+        assert_table!(
+            table,
+            "+-----------------+-----------+--------+"
+            "| name            | debit     | credit |"
+            "+-----------------+-----------+--------+"
+            "| Test JPY query  | 1 JPY     | 0 JPY  |"
+            "+-----------------+-----------+--------+"
+            "| Test JPY query  | 1 JPY     | 0 JPY  |"
+            "+-----------------+-----------+--------+"
+            "| Test SATS query | 1000 SATS | 0 SATS |"
+            "+-----------------+-----------+--------+"
+            "| Test SATS query | 1000 SATS | 0 SATS |"
+            "+-----------------+-----------+--------+"
+        );
+
         Ok(())
     }
 }
