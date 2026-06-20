bd9f08195b306fa4fd378ab5ea9e1d2efc5addba
haruki7049 <tontonkirikiri@gmail.com>
2026-06-20 20:30:30 +0900

    Update

--- crates/processor/src/prelude.rs
+++ crates/processor/src/prelude.rs
@@ -1,3 +1,1 @@
-pub use crate::types::configuration::*;
-pub use crate::types::currency::*;
-pub use crate::types::note::*;
+pub use crate::types::*;
--- crates/processor/src/types.rs
+++ crates/processor/src/types.rs
@@ -3,3 +3,7 @@
 pub mod configuration;
 pub mod currency;
 pub mod note;
+
+pub use configuration::*;
+pub use currency::*;
+pub use note::*;
