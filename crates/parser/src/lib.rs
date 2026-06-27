69f11a2e96b74643c5aa169a3d2757247b2a8d30
haruki7049 <tontonkirikiri@gmail.com>
2026-06-27 22:42:02 +0900

    Update

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,1 +1,15 @@
 //! Kakei parser
+
+use kakei_types::currency::Currency;
+use kakei_types::query::Query;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ParseError {}
+
+pub fn parse<C>(src: &str, currency: C) -> Result<Query<C>, ParseError>
+where
+    C: Currency,
+{
+    todo!()
+}
